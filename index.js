6acf40ffe769ae31687cb3ff7861cc3f1b507c0f
Ryland Sherman <rylandsherman1@gmail.com>
2023-11-01 11:43:30 -0600

    complete DOMContentLoaded assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const Element = document.querySelector('p');
+
+    if (Element) {
+      Element.textContent = "This is really cool!";
+    }
+
+  });
